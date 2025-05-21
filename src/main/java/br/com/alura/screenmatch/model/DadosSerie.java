890f291b9abbfb4ccc537a719be990620767dd0f
Big-Plato <luisoctavius.sc@gmail.com>
2025-05-21 18:35:46 -0300

    first commit

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating")String avaliacao) {
+}
+
+// Json@Alias somente lê
+// Json@Property traz o nome do campo também, com o foco tanto em serializar como deserializar
