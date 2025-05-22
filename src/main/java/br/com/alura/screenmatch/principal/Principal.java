d0df8bb245e56194c76b16c63307422d63d51926
Big-Plato <luisoctavius.sc@gmail.com>
2025-05-21 22:50:35 -0300

    refactor: separate menu from main

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,4 +1,20 @@
 package br.com.alura.screenmatch.principal;
 
+import java.util.Scanner;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+
 public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=60d4fcc6";
+    private ConsumoAPI consumo = new ConsumoAPI();
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série para busca: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(
+        ENDERECO + nomeSerie.replace(" ", "+") + API_KEY
+        );
+        // "https://www.omdbapi.com/?type=series&season=1&t=dexter&episode=1&apikey=60d4fcc6"
+    }
 }
